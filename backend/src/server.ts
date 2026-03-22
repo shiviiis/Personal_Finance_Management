0e60c4f0b1940aab16df3d058d10ac25d23352c7
shiviiis <shivangis200116@gmail.com>
2026-03-23 03:20:34 +0530

    Update CORS origin to production URL

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ import transactionRoutes from "./routes/transRoutes";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://personal-finance-management-beta.vercel.app/", credentials: true }));
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
 //     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
